ef03d78138950d10eddb8fada780cbcb27a99c63
a.vasiliev <a.vasiliev@uni-eng.ru>
2021-06-01 18:20:41 +0300

    Добавлено приложение

---
+++ MessangerClient/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MessangerClient.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MessangerClient
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindowViewModel viewModel = new MainWindowViewModel();
+
+            MainWindow view = new MainWindow();
+            view.DataContext = viewModel;
+
+
+            view.Show();
+        }
+    }
+}
---
+++ MessangerClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessangerClient.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Title { get; set; } = "Messanger Client";
+    }
+}
